ab8b8556d8840cf76bdd87ff0c5d31c140f7d0f2
Adrià González Fernández <adria.gonzalez.fernandez@everis.com>
2019-08-29 11:28:10 +0200

    GIT INIT

---
+++ LoginSwiftUI/LoginSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  LoginSwiftUI
+//
+//  Created by Adrià González Fernández on 29/08/2019.
+//  Copyright © 2019 Adrià González Fernández. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
